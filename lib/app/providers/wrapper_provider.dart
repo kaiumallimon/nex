2f2d07d9359e05bd11b2dc0509fa53cc78540837
Kaium Al Limon <limon.3cs@gmail.com>
2025-05-16 01:01:35 +0600

    Refactor WrapperProvider to lazily initialize PageController and ensure proper disposal. Update setCurrentIndex method to use the initialized controller for page navigation.

--- lib/app/providers/wrapper_provider.dart
+++ lib/app/providers/wrapper_provider.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class WrapperProvider extends ChangeNotifier {
-  final PageController _pageController = PageController();
+  PageController? _pageController;
 
-  PageController get pageController => _pageController;
+  PageController get pageController {
+    _pageController ??= PageController();
+    return _pageController!;
+  }
 
   int _currentIndex = 0;
 
@@ -11,11 +14,17 @@ class WrapperProvider extends ChangeNotifier {
 
   void setCurrentIndex(int index) {
     _currentIndex = index;
-    _pageController.animateToPage(
+    pageController.animateToPage(
       index,
       duration: Duration(milliseconds: 300),
       curve: Curves.easeInOut,
     );
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    super.dispose();
   }
 }
